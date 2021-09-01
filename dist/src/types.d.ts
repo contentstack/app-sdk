1422e6483865e5e235d449d8a2ba1ccb95c753e3
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-09-01 18:52:33 +0530

    build

--- dist/src/types.d.ts
+++ dist/src/types.d.ts
@@ -7,6 +7,9 @@ export declare interface ICustomField {
 export declare interface ISidebarWidget {
     [key: string]: any;
 }
+export declare interface IAssetSidebarWidget {
+    [key: string]: any;
+}
 export declare interface IRTE {
     [key: string]: any;
 }
@@ -108,6 +111,18 @@ export declare interface IFullScreenInitData {
         user: IUser;
     };
 }
+export declare interface ICurrentAsset {
+    [key: string]: any;
+}
+export declare interface IAssetSidebarInitData {
+    data: {
+        app_id: string;
+        stack: ICurrentStack;
+        type: 'ASSET_SIDEBAR_WIDGET';
+        user: IUser;
+        currentAsset: ICurrentAsset;
+    };
+}
 export declare interface IInitializationData {
     'CUSTOM_FIELD_WIDGET': IFieldConfig;
     'SIDEBAR_WIDGET': ISidebarInitData;
@@ -116,6 +131,7 @@ export declare interface IInitializationData {
     "STACK_CONFIG_WIDGET": IStackConfigInitData;
     "APP_CONFIG_WIDGET": IAppConfigInitData;
     "FULL_SCREEN_WIDGET": IFullScreenInitData;
+    "ASSET_SIDEBAR_WIDGET": IAssetSidebarInitData;
 }
-export declare type ILocation = "RTE_EXTENSION_WIDGET" | "CUSTOM_FIELD_WIDGET" | "DASHBOARD_WIDGET" | "SIDEBAR_WIDGET" | "STACK_CONFIG_WIDGET" | "APP_CONFIG_WIDGET" | "FULL_SCREEN_WIDGET";
+export declare type ILocation = "RTE_EXTENSION_WIDGET" | "CUSTOM_FIELD_WIDGET" | "DASHBOARD_WIDGET" | "SIDEBAR_WIDGET" | "STACK_CONFIG_WIDGET" | "APP_CONFIG_WIDGET" | "FULL_SCREEN_WIDGET" | "ASSET_SIDEBAR_WIDGET";
 //# sourceMappingURL=types.d.ts.map
