055a907b4ae56eb74d481a8e4f7ebef065055e94
Pritesh Jain <pritesh.jain@contentstack.com>
2021-12-13 02:10:18 +0530

    feat: add stack support on config widget

--- src/stack/index.ts
+++ src/stack/index.ts
@@ -135,6 +135,33 @@ class Stack {
     const options = { params: optionParams, action: 'getLocales' };
     return this._connection.sendToParent('stackQuery', options).then(onData).catch(onError);
   }
+
+  /**
+   * This API allows you to retrive a workflow of a stack using the {@link https://www.contentstack.com/docs/apis/content-management-api/#get-a-workflow| Language API} requests. Method returns a Promise object.
+   * @param {string} code Code of the desired locale
+   * @param {Object} params Optional parameters for the GET call
+   * @return {Object} A promise object which will be resolved with locale details.
+   */
+     getWorkflow(uid: string, params = {}) {
+      if (!uid) {
+        return Promise.reject(new Error('workflow uid is required'));
+      }
+      const options = { uid, params, action: 'getWorkflow' };
+      return this._connection.sendToParent('stackQuery', options).then(onData).catch(onError);
+    }
+  
+    /**
+     * This API allows you to retrive the locales of a stack using the {@link https://www.contentstack.com/docs/apis/content-management-api/#get-all-content-types| Languages API} requests. Method returns a Promise object.
+     * @param {Object} query Query for the GET call
+     * @param {Object} params Optional parameters for the GET call
+     * @return {Object} A Promise object which will be resolved with details of the locales.
+     */
+     getWorkflows(query = {}, params = {}) {
+      const optionParams: { [key: string]: any } = params;
+      optionParams.query = query;
+      const options = { params: optionParams, action: 'getWorkflows' };
+      return this._connection.sendToParent('stackQuery', options).then(onData).catch(onError);
+    }
 }
 
 export default Stack;
