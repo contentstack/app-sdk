aaef147d3dbe404a8ec6e05604d4fd617e1b3219
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2021-12-23 17:06:35 +0530

    add metadata support

--- src/metadata.ts
+++ src/metadata.ts
@@ -5,38 +5,67 @@ export declare interface IMetadata {
 }
 
 const metadataTypes = {
-    CREATE_METADATA: "CREATE_METADATA",
-    RETRIEVE_METADATA: "RETRIEVE_METADATA",
-    UPDATE_METADATA: "UPDATE_METADATA",
-    DELETE_METADATA: "DELETE_METADATA",
+    creteMetadata: "createMetadata",
+    retrieveMetadata: "retrieveMetadata",
+    updateMetadata: "updateMetadata",
+    deleteMetadata: "deleteMetadata",
 } as const;
 
 class Metadata {
     constructor(private _connection: any) {}
 
     createMetaData(metadataConfig: IMetadata) {
-        return this._connection.sendToParent(
-            metadataTypes.CREATE_METADATA,
-            metadataConfig
-        );
+        const { uid, type, ...otherMetaData } = metadataConfig;
+
+        if (!uid) throw new Error("uid is required");
+        if (!type) throw new Error("type is required");
+
+        const data = {
+            uid,
+            action: metadataTypes.creteMetadata,
+            payload: {
+                metadata: {
+                    type,
+                    ...otherMetaData,
+                },
+            },
+        };
+
+        return this._connection.sendToParent("stackQuery", data);
     }
 
     retrieveMetaData(type: "asset" | "entry" = "asset") {
         return this._connection.sendToParent(
-            metadataTypes.RETRIEVE_METADATA,
+            metadataTypes.retrieveMetadata,
             type
         );
     }
 
     updateMetaData(metadataConfig: IMetadata) {
-        return this._connection.sendToParent(
-            metadataTypes.UPDATE_METADATA,
-            metadataConfig
-        );
+        const { uid, type, _content_type_uid, ...otherMetaData } =
+            metadataConfig;
+
+        if (!uid) throw new Error("uid is required");
+        if (!type) throw new Error("type is required");
+        // type is entry and uid !== stackApiKey:  _content_type_uid is required
+
+        const data = {
+            uid,
+            action: metadataTypes.updateMetadata,
+            payload: {
+                metadata: {
+                    type,
+                    ...otherMetaData,
+                },
+            },
+        };
+
+        return this._connection.sendToParent("stackQuery", data);
     }
 
     deleteMetaData() {
-        return this._connection.sendToParent(metadataTypes.DELETE_METADATA);
+        //
+        return this._connection.sendToParent(metadataTypes.deleteMetadata);
     }
 }
 export default Metadata;
