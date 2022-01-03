b07f2aab0cd938d6b508b673ceee24019b52d72c
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2022-01-03 13:17:15 +0530

    renamed rte

--- src/types.ts
+++ src/types.ts
@@ -111,7 +111,7 @@ export declare interface IRTEInitData {
         app_id: string;
         installation_uid: string;
         stack: ICurrentStack;
-        type: "RTE_EXTENSION_WIDGET";
+        type: "RTE";
         user: IUser;
         config?: { [key: string]: any };
     };
@@ -198,14 +198,14 @@ export declare interface IInitializationData {
     FIELD: IFieldConfig;
     WIDGET: ISidebarInitData;
     DASHBOARD: IDashboardInitData;
-    RTE_EXTENSION_WIDGET: IRTEInitData;
+    RTE: IRTEInitData;
     APP_CONFIG_WIDGET: IAppConfigInitData;
     FULL_SCREEN_WIDGET: IFullScreenInitData;
     ASSET_SIDEBAR_WIDGET: IAssetSidebarInitData;
 }
 
 export declare type ILocation =
-    | "RTE_EXTENSION_WIDGET"
+    | "RTE"
     | "FIELD"
     | "DASHBOARD"
     | "WIDGET"
