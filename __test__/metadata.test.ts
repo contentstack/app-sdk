35deeecdb17ce843d1b63137d8d0a024177362a0
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2022-11-18 03:02:36 +0530

    test: metadata test cases

---
+++ __test__/metadata.test.ts
@@ -0,0 +1,72 @@
+import Metadata from "../src/metadata";
+
+describe("Metadata", () => {
+    let connection: { sendToParent: typeof jest.fn };
+    let sendToParent: typeof jest.fn;
+
+    beforeEach(() => {
+        sendToParent = jest.fn().mockReturnValue(Promise.resolve({ data: {} }));
+        connection = { sendToParent: sendToParent };
+        jest.spyOn(connection, "sendToParent");
+    });
+
+    test("should retrieve metadata", async () => {
+        const metadata = new Metadata(connection);
+        const uid = "some-uid";
+        const metadataConfig = { uid };
+        await metadata.retrieveMetaData(metadataConfig);
+        expect(connection.sendToParent).toHaveBeenCalledWith("stackQuery", {
+            uid,
+            action: "getMetadata",
+            payload: {
+                metadata: {
+                    uid,
+                },
+            },
+        });
+    });
+
+    test("should retrieve all metadata", async () => {
+        const metadata = new Metadata(connection);
+        const metadataConfig = { some: "config" };
+        await metadata.retrieveAllMetaData(metadataConfig);
+        expect(connection.sendToParent).toHaveBeenCalledWith("stackQuery", {
+            action: "getAllMetadata",
+            payload: {
+                ...metadataConfig,
+            },
+        });
+    });
+
+    test("should update metadata", async () => {
+        const metadata = new Metadata(connection);
+        const uid = "some-uid";
+        const metadataConfig = { uid, some: "config" };
+        await metadata.updateMetaData(metadataConfig);
+        expect(connection.sendToParent).toHaveBeenCalledWith("stackQuery", {
+            uid,
+            action: "updateMetadata",
+            payload: {
+                metadata: {
+                    ...metadataConfig,
+                },
+            },
+        });
+    });
+
+    test("should delete metadata", async () => {
+        const metadata = new Metadata(connection);
+        const uid = "some-uid";
+        const metadataConfig = { uid };
+        await metadata.deleteMetaData(metadataConfig);
+        expect(connection.sendToParent).toHaveBeenCalledWith("stackQuery", {
+            uid,
+            action: "deleteMetadata",
+            payload: {
+                metadata: {
+                    uid,
+                },
+            },
+        });
+    });
+});
