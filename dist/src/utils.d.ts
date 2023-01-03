90ba3d7d2478c62aff135b63759cb413bd8e2535
Ravi Lamkoti <ravilamkoti@gmail.com>
2023-01-03 12:13:31 +0530

    add: getPublishes and getReleases support

---
+++ dist/src/utils.d.ts
@@ -0,0 +1,5 @@
+export declare function onData(data: {
+    [key: string]: any;
+}): Promise<any>;
+export declare function onError(error: Error): Promise<never>;
+//# sourceMappingURL=utils.d.ts.map
