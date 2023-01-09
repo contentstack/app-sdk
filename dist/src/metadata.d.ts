1a824523889bc709f79b1bb5e0a12f26f9150e03
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-01-09 15:37:38 +0530

    chore: add dist

---
+++ dist/src/metadata.d.ts
@@ -0,0 +1,32 @@
+import { AnyObject } from "./types/common.types";
+export declare interface IMetadata {
+    uid: string;
+    type: "asset" | "entry";
+    _content_type_uid?: string;
+    locale?: string;
+    [key: string]: any;
+}
+export declare interface IMetadataCreateDto extends Omit<Partial<IMetadata>, "uid"> {
+    entity_uid: string;
+}
+export declare interface IMetadataUpdateDto {
+    uid: string;
+    [key: string]: any;
+}
+export declare interface IMetadataRetrieveDto {
+    uid: string;
+}
+export declare interface IMetadataDeleteDto {
+    uid: string;
+}
+declare class Metadata {
+    private _connection;
+    constructor(_connection: any);
+    createMetaData(metadataConfig: IMetadataCreateDto): any;
+    retrieveMetaData(metadataConfig: IMetadataRetrieveDto): any;
+    retrieveAllMetaData(params?: AnyObject): any;
+    updateMetaData(metadataConfig: IMetadataUpdateDto): any;
+    deleteMetaData(metadataConfig: IMetadataDeleteDto): any;
+}
+export default Metadata;
+//# sourceMappingURL=metadata.d.ts.map
