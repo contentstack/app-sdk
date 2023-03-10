3c5305e9823f676d9191864c4dc43b1ba9585440
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2023-03-10 12:17:29 +0530

    test: add tests for field location

---
+++ __test__/entryFieldLocation/frame.test.ts
@@ -0,0 +1,180 @@
+import EntryFieldLocationFrame from "../../src/entryFieldLocation/frame";
+import { errorMessage } from "../../src/utils/errorMessages";
+import testData from "../data/testData.json";
+
+describe("EntryFieldLocationFrame", () => {
+    let frameInstance: EntryFieldLocationFrame;
+    let sendToParent: any;
+    let connection: { sendToParent: (...props: any[]) => any };
+
+    let emitter: any;
+
+    beforeEach(() => {
+        sendToParent = jest.fn();
+        connection = { sendToParent };
+
+        emitter = {
+            on: (_event: any, cbf: (...props: any[]) => void) => {
+                setTimeout(() => {
+                    cbf({ data: { data: testData.entry, name: "entrySave" } });
+                    cbf({ data: { data: {}, name: "entryPublish" } });
+                    cbf({
+                        data: { data: testData.entry, name: "entryChange" },
+                    });
+                }, 50);
+            },
+        };
+
+        jest.spyOn(emitter, "on");
+        frameInstance = new EntryFieldLocationFrame(connection, emitter);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("updateDimension", () => {
+        it("should calculate the dimension of the frame if not provided", async () => {
+            await frameInstance.updateDimension();
+
+            const { width, height } =
+                document.documentElement.getBoundingClientRect();
+
+            expect(sendToParent).toHaveBeenLastCalledWith("resize", {
+                height,
+                width,
+            });
+        });
+        it("should update the height if the height is provided", async () => {
+            await frameInstance.updateDimension({ height: 100 });
+
+            expect(sendToParent).toHaveBeenLastCalledWith("resize", {
+                height: 100,
+            });
+        });
+        it("should update the width if the width is provided", () => {
+            frameInstance.updateDimension({ width: 100 });
+
+            expect(sendToParent).toHaveBeenLastCalledWith("resize", {
+                width: 100,
+            });
+        });
+        it("should not update the height if the value is same as previous", async () => {
+            await frameInstance.updateDimension({ height: 100 });
+            await frameInstance.updateDimension({ height: 100 });
+
+            expect(sendToParent).toHaveBeenCalledTimes(1);
+        });
+        it("should not update the width if the value is same as previous", async () => {
+            await frameInstance.updateDimension({ width: 100 });
+            await frameInstance.updateDimension({ width: 100 });
+
+            expect(sendToParent).toHaveBeenCalledTimes(1);
+        });
+        it("should throw an error if the height if the value is not a number", async () => {
+            await expect(
+                // @ts-ignore
+                frameInstance.updateDimension({ height: "100" })
+            ).rejects.toThrowError(
+                errorMessage.entryField.frame.dimensionHeightShouldBeNumber
+            );
+        });
+
+        it("should not update the width if the value is not a number", async () => {
+            await expect(
+                // @ts-ignore
+                frameInstance.updateDimension({ width: "100" })
+            ).rejects.toThrowError(
+                errorMessage.entryField.frame.dimensionWidthShouldBeNumber
+            );
+        });
+    });
+
+    describe("enableAutoResizing", () => {
+        it("should run autoResizing when setup is called", () => {
+            const existingMutationObserver = window.MutationObserver;
+
+            const mutationObserverMock = jest.fn(function MutationObserver(
+                callback
+            ) {
+                this.observe = jest.fn();
+                this.disconnect = jest.fn();
+                // Optionally add a trigger() method to manually trigger a change
+                this.trigger = (mockedMutationsList) => {
+                    callback(mockedMutationsList, this);
+                };
+            });
+
+            //@ts-ignore
+            global.MutationObserver = mutationObserverMock;
+
+            frameInstance = new EntryFieldLocationFrame(connection, emitter);
+
+            const [observerInstance] = <void[] | [{ trigger: () => {} }]>(
+                mutationObserverMock.mock.instances
+            );
+
+            observerInstance?.trigger();
+
+            const { width, height } =
+                document.documentElement.getBoundingClientRect();
+
+            expect(sendToParent).toHaveBeenLastCalledWith("resize", {
+                height,
+                width,
+            });
+
+            expect(sendToParent).toHaveBeenCalledTimes(1);
+
+            frameInstance.enableAutoResizing();
+
+            window.MutationObserver = existingMutationObserver;
+        });
+    });
+
+    describe("disableAutoResizing", () => {
+        it("should remove the listener", () => {
+            const existingMutationObserver = window.MutationObserver;
+
+            const mutationObserverMock = jest.fn(function MutationObserver(
+                callback
+            ) {
+                this.observe = jest.fn();
+                this.disconnect = jest.fn();
+                // Optionally add a trigger() method to manually trigger a change
+                this.trigger = (mockedMutationsList) => {
+                    callback(mockedMutationsList, this);
+                };
+            });
+
+            //@ts-ignore
+            global.MutationObserver = mutationObserverMock;
+
+            frameInstance = new EntryFieldLocationFrame(connection, emitter);
+
+            frameInstance.disableAutoResizing();
+
+            sendToParent.mockClear();
+
+            const [observerInstance] = <void[] | [{ trigger: () => {} }]>(
+                mutationObserverMock.mock.instances
+            );
+
+            observerInstance?.trigger();
+
+            const { width, height } =
+                document.documentElement.getBoundingClientRect();
+
+            expect(sendToParent).toHaveBeenLastCalledWith("resize", {
+                height,
+                width,
+            });
+
+            expect(sendToParent).toHaveBeenCalledTimes(1);
+
+            frameInstance.disableAutoResizing();
+
+            window.MutationObserver = existingMutationObserver;
+        });
+    });
+});
---
+++ src/utils/errorMessages.ts
@@ -0,0 +1,12 @@
+export const errorMessage = {
+    entryField: {
+        entry: {
+            tagsShouldNotBeBlank: "Tags cannot be blank",
+            tagsShouldBeArrayOfStrings: "Tags must be an array of strings",
+        },
+        frame: {
+            dimensionHeightShouldBeNumber: "Dimension height must be a number",
+            dimensionWidthShouldBeNumber: "Dimension width must be a number",
+        },
+    },
+};
