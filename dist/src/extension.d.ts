ed4da93f5cf796d0eccfbc050371eb762317b07f
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-04-04 18:58:34 +0530

    chore: update build

--- dist/src/extension.d.ts
+++ dist/src/extension.d.ts
@@ -1,78 +1,46 @@
 import Stack from "./stack";
 import Store from "./store";
 import Metadata from "./metadata";
-import {
-  IAppConfigInitData,
-  IAppConfigWidget,
-  IAssetSidebarInitData,
-  ICustomField,
-  IDashboardInitData,
-  IDashboardWidget,
-  IFieldInitData,
-  IFieldModifierLocation,
-  IFieldModifierLocationInitData,
-  ILocation,
-  IPageWidget,
-  IRTEInitData,
-  ISidebarInitData,
-  ISidebarWidget,
-  IUser,
-  IFullPageLocationInitData,
-  IFullPageLocation,
-  IEntryFieldLocation,
-  IEntryFieldLocationInitData,
-} from "./types";
+import { IAppConfigInitData, IAppConfigWidget, IAssetSidebarInitData, ICustomField, IDashboardInitData, IDashboardWidget, IFieldInitData, IFieldModifierLocation, IFieldModifierLocationInitData, ILocation, IPageWidget, IRTEInitData, ISidebarInitData, ISidebarWidget, IUser, IFullPageLocationInitData, IFullPageLocation, IEntryFieldLocation, IEntryFieldLocationInitData } from "./types";
 import { IRTEPluginInitializer } from "./RTE/types";
 import AssetSidebarWidget from "./AssetSidebarWidget";
 /** Class representing an extension from Contentstack App Framework SDK. */
 declare class Extension {
-  /**
-   * @hideconstructor
-   */
-  appUID: string;
-  installationUID: string;
-  currentUser: IUser;
-  private type;
-  private config;
-  postRobot: any;
-  stack: Stack;
-  store: Store;
-  metadata: Metadata;
-  locationUID: string;
-  location: {
-    DashboardWidget: IDashboardWidget | null;
-    SidebarWidget: ISidebarWidget | null;
-    CustomField: ICustomField | null;
-    RTEPlugin: IRTEPluginInitializer | null;
-    AppConfigWidget: IAppConfigWidget | null;
-    FullscreenAppWidget: IPageWidget | null;
-    AssetSidebarWidget: AssetSidebarWidget | null;
-    EntryFieldLocation: IEntryFieldLocation | null;
-    FullPage: IFullPageLocation | null;
-    FieldModifierLocation: IFieldModifierLocation | null;
-  };
-  constructor(
-    initData:
-      | IRTEInitData
-      | IDashboardInitData
-      | IFieldInitData
-      | ISidebarInitData
-      | IAppConfigInitData
-      | IAssetSidebarInitData
-      | IEntryFieldLocationInitData
-  );
-  pulse: (
-    eventName: string,
-    metadata: {
-      [key: string]: any;
-    }
-  ) => void;
-  getConfig: () => Promise<{
-    [key: string]: any;
-  }>;
-  getCurrentLocation: () => ILocation;
-  static initialize(version: string): any;
-  setReady(): any;
+    /**
+     * @hideconstructor
+     */
+    appUID: string;
+    installationUID: string;
+    currentUser: IUser;
+    private type;
+    private config;
+    postRobot: any;
+    stack: Stack;
+    store: Store;
+    metadata: Metadata;
+    locationUID: string;
+    location: {
+        DashboardWidget: IDashboardWidget | null;
+        SidebarWidget: ISidebarWidget | null;
+        CustomField: ICustomField | null;
+        RTEPlugin: IRTEPluginInitializer | null;
+        AppConfigWidget: IAppConfigWidget | null;
+        FullscreenAppWidget: IPageWidget | null;
+        AssetSidebarWidget: AssetSidebarWidget | null;
+        EntryFieldLocation: IEntryFieldLocation | null;
+        FullPage: IFullPageLocation | null;
+        FieldModifierLocation: IFieldModifierLocation | null;
+    };
+    constructor(initData: IRTEInitData | IDashboardInitData | IFieldInitData | ISidebarInitData | IAppConfigInitData | IAssetSidebarInitData | IFullPageLocationInitData | IEntryFieldLocationInitData | IFieldModifierLocationInitData);
+    pulse: (eventName: string, metadata: {
+        [key: string]: any;
+    }) => void;
+    getConfig: () => Promise<{
+        [key: string]: any;
+    }>;
+    getCurrentLocation: () => ILocation;
+    static initialize(version: string): any;
+    setReady(): any;
 }
 export default Extension;
 //# sourceMappingURL=extension.d.ts.map
