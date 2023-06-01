c6b65b96ba77497b45f6b22ed65d2885dcb27ec6
Jay Chouhan <jaychouhan@Jays-MacBook-Pro.local>
2023-06-01 20:20:52 +0530

    test: getappversion method test cases added

--- __test__/extension.test.ts
+++ __test__/extension.test.ts
@@ -4,9 +4,33 @@ import Extension from "../src/extension";
 import { IAppConfigInitData, Region } from "../src/types";
 
 jest.mock("post-robot", () => ({
-    sendToParent: jest.fn(),
-    on: jest.fn(),
+    __esModule: true,
+    default: {
+        on: jest.fn(),
+        sendToParent: jest
+            .fn()
+            .mockReturnValue(Promise.resolve({ data: { version: 90 } })),
+    },
 }));
+
+const manifest = {
+    created_by: {
+        uid: "string",
+        first_name: "string",
+        last_name: "string",
+    },
+    icon: "string",
+    name: "string",
+    target_type: "string",
+    uid: "string",
+    updated_by: {
+        uid: "string",
+        first_name: "string",
+        last_name: "string",
+    },
+    version: 5,
+    visibility: "string",
+};
 
 const initData: IAppConfigInitData = {
     data: {
@@ -30,6 +54,53 @@ const initData: IAppConfigInitData = {
         },
         user: {},
         currentBranch: "currentBranch",
+        manifest: manifest,
+    },
+};
+
+const initDataWithoutManifest: IAppConfigInitData = {
+    data: {
+        type: "APP_CONFIG_WIDGET",
+        app_id: "app_id",
+        installation_uid: "installation_uid",
+        extension_uid: "extension_uid",
+        region: "NA",
+        stack: {
+            created_at: "created_at",
+            updated_at: "updated_at",
+            uid: "uid",
+            org_uid: "org_uid",
+            api_key: "api_key",
+            master_locale: "master_locale",
+            is_asset_download_public: true,
+            owner_uid: "owner_uid",
+            user_uids: ["user_uids"],
+            settings: {},
+            name: "name",
+        },
+        user: {},
+        currentBranch: "currentBranch",
+    },
+};
+
+const initDataJsonRte = {
+    data: {
+        type: "RTE",
+        region: "NA",
+        stack: {
+            created_at: "created_at",
+            updated_at: "updated_at",
+            uid: "uid",
+            org_uid: "org_uid",
+            api_key: "api_key",
+            master_locale: "master_locale",
+            is_asset_download_public: true,
+            owner_uid: "owner_uid",
+            user_uids: ["user_uids"],
+            settings: {},
+            name: "name",
+        },
+        user: {},
     },
 };
 
@@ -81,6 +152,31 @@ describe("Extension", () => {
             const extensionObj = new Extension(initData);
             const region = extensionObj.getCurrentRegion();
             expect(region).toBe(Region.NA);
+        });
+    });
+
+    describe("getAppVersion", () => {
+        it("should have getAppVersion method", () => {
+            const extensionObj = new Extension(initData);
+            expect(extensionObj.getAppVersion).toBeDefined();
+        });
+
+        it("should return a app version", async () => {
+            const extensionObj = new Extension(initData);
+            const version = await extensionObj.getAppVersion();
+            expect(version).toBe(5);
+        });
+
+        it("should return null for data without installation uid & manifest, this case is for jsonRte", async () => {
+            const extensionObj = new Extension(initDataJsonRte as any);
+            const version = await extensionObj.getAppVersion();
+            expect(version).toBe(null);
+        });
+
+        it("should execute the function without errors which calls postRobo init, this case is for customField where we dont get the manifest", async () => {
+            const extensionObj = new Extension(initDataWithoutManifest);
+            const version = await extensionObj.getAppVersion();
+            expect(version).toBe(90);
         });
     });
 });
