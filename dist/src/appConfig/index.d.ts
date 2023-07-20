fc8681f9a212029f47392f6ed78df1bd53a33107
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-07-20 13:43:00 +0530

    feat!: refactor method response to clean up post robot response

--- dist/src/appConfig/index.d.ts
+++ dist/src/appConfig/index.d.ts
@@ -18,12 +18,12 @@ export declare class AppConfig {
     setInstallationData: (installationData: IInstallationData) => Promise<GenericObjectType>;
     getInstallationData: () => Promise<IInstallationData>;
     /**
-     * Set the validation state of the app. If the validation is false, the Contentstack App Config
-     * will not allow to save the configuration. The message will be displayed if provided.
-     * @param isValid set the validation state of the app
-     * @param options additional options to be sent to the parent
-     * @returns returns a promise with the data sent from the parent
+     * Sets the validation state of the app. If the validation is false, the Contentstack App Config
+     * will not allow saving the configuration. The message will be displayed if provided.
+     * @param {boolean} isValid - The validation state of the app.
+     * @param {object} options - Additional options to be sent to the parent.
+     * @returns {Promise<void>} - A promise that resolves to void.
      */
-    setValidity(isValid: boolean, options?: ValidationOptions): Promise<Record<string, any>>;
+    setValidity(isValid: boolean, options?: ValidationOptions): Promise<void>;
 }
 //# sourceMappingURL=index.d.ts.map
