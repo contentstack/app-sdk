fe5aa442cffba0af2c7926c63d15c821ae789e07
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-12-14 15:32:13 +0530

    chore: update maintainers and remove build files

--- dist/src/types/common.types.d.ts
+++
@@ -1,5 +0,0 @@
-export interface AnyProperty {
-    [propName: string]: any;
-}
-export declare type GenericObjectType = Record<string, any>;
-//# sourceMappingURL=common.types.d.ts.map
--- dist/src/utils/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { Region } from "../types";
-export declare function onData<Data extends Record<string, any>>(data: {
-    data: Data;
-}): Promise<Data>;
-export declare function onError(error: Error): Promise<never>;
-export declare function formatAppRegion(region: string): Region;
-export declare function getPreferredBodyElement(nodeCollection: HTMLCollection): Element;
-//# sourceMappingURL=utils.d.ts.map
