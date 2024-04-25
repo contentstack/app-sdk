7a91e62218f8d082a4a093444bcb6038e6aa8839
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2024-04-25 12:38:00 +0530

    5 commit

--- src/variantGroup/index.ts
+++ src/variantGroup/index.ts
@@ -1,43 +1,13 @@
 import Base from "../stack/api/base";
-import {
-    BranchDetail,
-    GetAllStacksOptions,
-    StackAdditionalData,
-    StackDetail,
-    StackSearchQuery,
-} from "../types/stack.types";
 
 /**
  * Class representing the variant group.
  */
 
 let connection = {};
-class VariantGroup {
-
-    /**
-     * @hideconstructor
-     */
-
-    _connection: any;
-    _data: StackDetail;
-    private _currentBranch: BranchDetail | null = null;
-
-    constructor(
-        data: StackDetail = {} as StackDetail,
-        connection: any,
-        additionalData: StackAdditionalData
-    ) {
-        this._connection = connection;
-        this._data = data;
-
-        const currentBranch = additionalData.currentBranch || "";
-
-        if (currentBranch) {
-            this._currentBranch =
-                (data.branches || []).find(
-                    (branch) => branch.uid === additionalData.currentBranch
-                ) || null;
-        }
+class VariantGroup extends Base {
+    constructor(uid: string) {
+        super(uid);
     }
 
     createVariantGroup() {
@@ -49,7 +19,7 @@ export default (uiConnection: any) => {
     connection = uiConnection;
     return new Proxy(VariantGroup, {
         apply(Target: any, thisArg, argumentsList: any[]) {
-            return new Target(...argumentsList);  
+            return new Target(...argumentsList);
         },
     });
 };
