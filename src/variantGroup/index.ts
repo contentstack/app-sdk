22f5eb2f8054e578b97f2d6db327170584e56488
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2024-04-25 13:56:08 +0530

    10 commit

--- src/variantGroup/index.ts
+++ src/variantGroup/index.ts
@@ -1,10 +1,11 @@
 import Base from "../stack/api/base";
 
 /**
- * Class representing the variant group.
+ * Class representing the Variant Group.
  */
 
 let connection = {};
+
 class VariantGroup extends Base {
     constructor(uid: string) {
         if (!uid) {
@@ -12,17 +13,34 @@ class VariantGroup extends Base {
         }
         super(uid);
         this._query = {};
-        return this
+        return this;
     }
 
     static get connection() {
         return connection;
     }
 
-    getGroupedVariants() {
-        console.log("getGroupedVariants",this.uid);
-        this._query.group_uid = this.uid
-        return this.fetch("getGroupedVariants");
+    /**
+     * @function
+     * @name Stack#VariantGroup#getVariantsByGroup
+     * @description This method returns all the variants within a group.
+     * @example appSDK.stack.VariantGroup("variant_group_uid").getVariantsByGroup();
+     * @return {external:Promise}
+     */
+    getVariantsByGroup() {
+        return this.fetch("getVariantsByGroup");
+    }
+
+    /**
+     * @function
+     * @name Stack#VariantGroup#getVariantsByGroup#deleteVariant
+     * @description This method deletes a specified variant from a group.
+     * @example appSDK.stack.VariantGroup("variant_group_uid").deleteVariant("variant_uid");
+     * @return {external:Promise}
+     */
+    deleteVariant(variant_uid: string) {
+        this._query.variant_uid = variant_uid;
+        return this.fetch("deleteVariant");
     }
 }
 
