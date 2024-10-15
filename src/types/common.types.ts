a2eafdf75f83c918cc2e6bb5c90db6364e662509
Amitkanswal <amitkanswal7@gmail.com>
2024-10-15 11:51:01 +0530

    feat:integrated js management-sdk

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -3,3 +3,25 @@ export interface AnyProperty {
 }
 
 export declare type GenericObjectType = Record<string, any>;
+
+/**
+ * Standard HTTP method strings
+ */
+type _HTTPMethods = 'DELETE' | 'GET' | 'HEAD' | 'PATCH' | 'POST' | 'PUT' | 'OPTIONS' |
+'PROPFIND' | 'PROPPATCH' | 'MKCOL' | 'COPY' | 'MOVE' | 'LOCK' | 'UNLOCK' | 'TRACE' | 'SEARCH' | 'REPORT' | 'MKCALENDAR'
+
+export type HTTPMethods = Uppercase<_HTTPMethods> | Lowercase<_HTTPMethods>
+
+export type ApiRequestProps = {
+        method: HTTPMethods,
+        url: string,
+        headers?: GenericObjectType,
+        body?: unknown,
+        params?:GenericObjectType
+        multipart_data?: {
+          [key:string]: {
+            "type": string,
+            "value": any
+          }
+        }
+}
--- src/utils/adapter.ts
+++ src/utils/adapter.ts
@@ -1,6 +1,40 @@
-import { ApiRequestProps } from '../types/stack.types';
-import { onData, onError } from "./utils";
 import PostRobot from "post-robot";
+import { onData, onError } from "./utils";import { AxiosRequestConfig, AxiosResponse } from '../types/axios.type';
+import { ApiRequestProps, GenericObjectType } from '../types/common.types';
+
+export function createSDKAdapter(postRobot: typeof PostRobot): (config: AxiosRequestConfig) => Promise<AxiosResponse<GenericObjectType>> {
+  return async (config: AxiosRequestConfig): Promise<AxiosResponse<GenericObjectType>> => {
+    const req: ApiRequestProps = {
+      url: config?.url,
+      method: config?.method,
+      headers: config?.headers,
+      body: config?.data,
+      params: config?.params,
+    };
+
+    try {
+      const data = await dispatchPostRobotRequest(postRobot, req) as GenericObjectType;
+      return {
+        data,
+        status: data.status || 200,
+        statusText: 'OK',
+        headers: config.headers || {},
+        config,
+        request: req,
+      };
+    } catch (error) {
+      const typedError = error as GenericObjectType & { status?: number; statusText?: string; headers?: Record<string, string> };
+      return {
+        data: typedError,
+        status: typedError.status || 500,
+        statusText: typedError.statusText || 'Internal Server Error',
+        headers: typedError.headers || {},
+        config,
+        request: req,
+      };
+    }
+  };
+}
 
 export function dispatchPostRobotRequest(postRobot: typeof PostRobot, opts:ApiRequestProps):Promise<any> {
     return postRobot
