de84acef58812f6b1376017d2d1b0a787dae45d5
praveen-mohan-cs <praveen.mohan@contentstack.com>
2024-10-21 15:39:21 +0530

    Added Content type sidebar UI location

--- src/uiLocation.ts
+++ src/uiLocation.ts
@@ -2,6 +2,7 @@ import postRobot from "post-robot";
 import EventEmitter from "wolfy87-eventemitter";
 
 import AssetSidebarWidget from "./AssetSidebarWidget";
+import ContentTypeSidebarWidget from "./ContentTypeSidebarWidget";
 import { IRTEPluginInitializer } from "./RTE/types";
 import { AppConfig } from "./appConfig";
 import Entry from "./entry";
@@ -114,6 +115,7 @@ class UiLocation {
         AssetSidebarWidget: AssetSidebarWidget | null;
         FullPage: IFullPageLocation | null;
         FieldModifierLocation: IFieldModifierLocation | null;
+        ContentTypeSidebarWidget: ContentTypeSidebarWidget | null;
     };
 
     constructor(initData: InitializationData) {
@@ -151,6 +153,7 @@ class UiLocation {
             AssetSidebarWidget: null,
             FullPage: null,
             FieldModifierLocation: null,
+            ContentTypeSidebarWidget: null,
         };
 
         window["postRobot"] = postRobot;
@@ -258,6 +261,16 @@ class UiLocation {
                 break;
             }
 
+            case LocationType.CONTENT_TYPE_SIDEBAR_WIDGET: {
+                this.location.ContentTypeSidebarWidget =
+                    new ContentTypeSidebarWidget(
+                        initializationData,
+                        postRobot,
+                        emitter
+                    );
+                break;
+            }
+
             case LocationType.FIELD:
             default: {
                 initializationData.self = true;
@@ -342,6 +355,15 @@ class UiLocation {
 
                 if (event.data.name === "extensionFieldChange") {
                     emitter.emitEvent("extensionFieldChange", [
+                        { data: event.data.data },
+                    ]);
+                }
+
+                if (event.data.name === "contentTypeSave") {
+                    emitter.emitEvent("contentTypeSave", [
+                        { data: event.data.data },
+                    ]);
+                    emitter.emitEvent("updateFields", [
                         { data: event.data.data },
                     ]);
                 }
