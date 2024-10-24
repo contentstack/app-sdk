b2ff68d26b63131b6c26f487570ae65e6720e39c
Amitkanswal <amitkanswal7@gmail.com>
2024-10-24 19:14:11 +0530

    ref: refactor api method
    Signed-off-by: Amitkanswal <amitkanswal7@gmail.com>

---
+++ src/types/api.type.ts
@@ -0,0 +1,21 @@
+import { HTTPMethods } from './common.types';
+
+export interface ApiRequestParams {
+  url: string;
+  method: HTTPMethods;
+  baseURL?: string;
+  headers?: Record<string, string>;
+  data?: unknown;
+  [key: string]: any;
+}
+
+export interface ApiResponse<T = any> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: Record<string, string>;
+  config: ApiRequestParams;
+  request?: any;
+}
+
+export type GenericAdapter = (config: ApiRequestParams) => Promise<ApiResponse>;
