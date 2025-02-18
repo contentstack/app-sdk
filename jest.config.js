8438c9e84f546f37d90f35f4b29d8feda5a17dd3
Amitkanswal <amitkanswal7@gmail.com>
2025-02-18 15:54:52 +0530

    fix:update api implementation
    Signed-off-by: Amitkanswal <amitkanswal7@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
     testEnvironment: "jsdom",
     transform: {
         "^.+\\.(ts|tsx)$": "ts-jest",
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    moduleNameMapper: {
+      "^axios$": "axios/dist/node/axios.cjs"
     },
     collectCoverageFrom: ["./src/**"],
     coveragePathIgnorePatterns: ["<rootDir>.*types.ts"],
