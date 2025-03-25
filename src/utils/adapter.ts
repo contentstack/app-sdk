9976df28b28814743e713d5fa882e621db4258b6
Amitkanswal <amitkanswal7@gmail.com>
2025-03-25 13:14:43 +0530

    feat:error handling and new region endpoint support
    Signed-off-by: Amitkanswal <amitkanswal7@gmail.com>

--- src/utils/adapter.ts
+++ src/utils/adapter.ts
@@ -1,47 +1,50 @@
-import PostRobot from 'post-robot';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
-import { onError, fetchToAxiosConfig } from './utils';
+import PostRobot from "post-robot";
+import { Response } from 'node-fetch';
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+
+import { onError, fetchToAxiosConfig, serializeAxiosResponse, handleApiError, sanitizeResponseHeader } from "./utils";
 
 /**
  * Dispatches a request using PostRobot.
  * @param postRobot - The PostRobot instance.
  * @returns A function that takes AxiosRequestConfig and returns a promise.
  */
-export const dispatchAdapter = (postRobot: typeof PostRobot) => (config: AxiosRequestConfig)=> {
+export const dispatchAdapter = (postRobot: typeof PostRobot) => (config: AxiosRequestConfig): Promise<AxiosResponse> => {
   return postRobot
-    .sendToParent("apiAdapter",  config )
-    .then(({ data }) => ({ ...data, config }))
+    .sendToParent("apiAdapter", config)
+    .then((event:unknown) => {
+      const { data } = event as { data: AxiosResponse };
+      if (data.status >= 400) {
+        throw serializeAxiosResponse(data, config);
+      }
+      return serializeAxiosResponse(data, config);
+    })
     .catch(onError);
 };
-
 /**
  * Dispatches an API request using axios and PostRobot.
  * @param url - The URL of the API endpoint.
  * @param options - Optional request options.
  * @returns A promise that resolves to a partial Response object.
  */
-export const dispatchApiRequest = async (url: string, options?: RequestInit): Promise<Response> => {
-  try {
-    const config = fetchToAxiosConfig(url, options);
-    const responseData = await dispatchAdapter(PostRobot)(config) as AxiosResponse; 
-    return new Response(responseData.data,{
-      status: responseData.status,
-      statusText: responseData.statusText,
-      headers: new Headers(responseData.config.headers || {}),
-    });
-
-  } catch (error: any) {
-    if (error.response) {
-      const fetchResponse = new Response(error.response.data, {
-          status: error.response.status,
-          statusText: error.response.statusText,
-          headers: new Headers(error.response.headers) 
-      });
-      return Promise.reject(fetchResponse);
-    } else if (error.request) {
-        return Promise.reject(new Response(null, { status: 0, statusText: 'Network Error' }));
-    } else {
-        return Promise.reject(new Response(null, { status: 0, statusText: error.message }));
-    }
+export const dispatchApiRequest = async (
+    url: string,
+    options?: RequestInit,
+): Promise<Response> => {
+    try {
+        const config = fetchToAxiosConfig(url, options);
+        const responseData = (await dispatchAdapter(PostRobot)(
+            config
+        )) as AxiosResponse;
+        
+        return new Response(responseData?.data, {
+            status: responseData.status,
+            statusText: responseData.statusText,
+            url: responseData.config.url,
+            headers: new Headers(sanitizeResponseHeader(responseData.config.headers || {})),
+        });
+        
+    } catch (error) {
+     return handleApiError(error);
   }
 };
